6b3595cbdab130658b2046ac043e37bbe3f93387
arpit <arpit@swarajfinpro.com>
2022-12-14 11:08:31 +0530

    fix bug

--- dashboard.php
+++ dashboard.php
@@ -647,6 +647,7 @@ if (mysqli_num_rows($result)) {
                 </div>
             </div>
         </div>
+        <div class="text-center"><a class="btn btn-primary m-2 p-2 btn-lg" href="roadMap.php">Road Map</a></div>
     </fieldset>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
